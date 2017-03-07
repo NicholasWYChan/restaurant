4328f8518a14237cd23b2522acac0f765ea6df57
NicholasWYChan <bitbostonnick@gmail.com>
2017-03-07 15:40:06 -0500

    Add files via upload

---
+++ js/restaurant.js
@@ -0,0 +1,17 @@
+(function($){
+
+$(document).ready(function(){
+	alert='whats happneing?';
+	$('.introPic').click(function(){
+    	$('#intro','.introPic').fadeOut('fast');
+    	$('#home').fadeIn('fast');
+  	}); 
+/*
+	$('.guestSelect').click(function(){
+    	$('#home').fadeOut('');
+    	$('#mattis').fadeIn('');
+  });*/
+  
+});
+
+});
